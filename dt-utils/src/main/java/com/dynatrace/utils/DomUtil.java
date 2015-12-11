23de2044bd6599c6da329af43e403e8e7b475991
Dynatrace-Reinhard-Pilz <reinhard.pilz@dynatrace.com>
2015-12-11 14:09:15 -0500

    Suppressing SaxParser Log Output

--- dt-utils/src/main/java/com/dynatrace/utils/DomUtil.java
+++ dt-utils/src/main/java/com/dynatrace/utils/DomUtil.java
@@ -13,11 +13,13 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
-import com.dynatrace.utils.Version;
-
-public class DomUtil {
+public class DomUtil implements ErrorHandler {
+	
+	private static final ErrorHandler ERROR_HANDLER = new DomUtil();
 	
 	public static Document build(byte[] bytes) throws IOException {
 		try (InputStream in = new ByteArrayInputStream(bytes)) {
@@ -41,6 +43,7 @@ public class DomUtil {
 		try {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			dBuilder.setErrorHandler(ERROR_HANDLER);
 			return dBuilder.parse(in);
 		} catch (ParserConfigurationException | SAXException e) {
 			throw new IOException(e);
@@ -62,6 +65,21 @@ public class DomUtil {
 		} catch (IllegalArgumentException e) {
 			throw new IOException(fileName + " is not a System Profile (version attribute invalid)", e);
 		}
+	}
+
+	@Override
+	public void warning(SAXParseException exception) throws SAXException {
+		// ingore
+	}
+
+	@Override
+	public void error(SAXParseException exception) throws SAXException {
+		// ingore
+	}
+
+	@Override
+	public void fatalError(SAXParseException exception) throws SAXException {
+		// ingore
 	}
 	
 }
